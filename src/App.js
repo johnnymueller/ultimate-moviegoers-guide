04bbcab296d0b8b1de52fef96a82c0c90588bbfc
jjjohn <johnmueller@fastmail.com>
2018-10-31 12:50:43 -0600

    Scss and router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import './App.scss';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <Header />
+
+      <Route exact path="/" component={Dashboard} />
+      <Route path="/time" component={Time} />
+      <Route path="/invoices" component={Invoices} />
+      <Route path="/expenses" component={Expenses} />
+      <Route path="/topics" component={Topics} />
+    </div>
+  </Router>
+);
+
+const Dashboard = () => <h2>Dashboard</h2>;
+const Time = () => <h2>Time</h2>;
+const Invoices = () => <h2>Invoices</h2>;
+const Expenses = () => <h2>Expenses</h2>;
+const Topic = ({ match }) => <h3>Requested Param: {match.params.id}</h3>;
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+
+    <ul>
+      <li>
+        <Link to={`${match.url}/components`}>Components</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.path}/:id`} component={Topic} />
+    <Route
+      exact
+      path={match.path}
+      render={() => <h3>Please select a topic.</h3>}
+    />
+  </div>
+);
+const Header = () => (
+  <ul id="nav">
+    <li>
+      <Link to="/">Dashboard</Link>
+    </li>
+    <li>
+      <Link to="/time">Time</Link>
+    </li>
+    <li>
+      <Link to="/invoices">Invoices</Link>
+    </li>
+    <li>
+      <Link to="/expenses">Expenses</Link>
+    </li>
+    <li>
+      <Link to="/topics">Topics</Link>
+    </li>
+  </ul>
+);
 
 export default App;
