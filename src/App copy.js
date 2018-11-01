09612d51716245215fe26145b9e96011a4c65740
jjjohn <johnmueller@fastmail.com>
2018-11-01 15:10:59 -0600

    routing

--- src/App copy.js
+++ src/App copy.js
@@ -48,8 +48,6 @@ class App extends React.Component {
         // always executed
       });
 
-    // highest rated: certification_country=US&certification=R&sort_by=vote_average.desc
-    // in theater: /discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22
     axios.get('https://api.themoviedb.org/3/discover/movie', {
         params: {
           api_key: '4268bd97bf4cfe10c3797b864eef07b8',
@@ -88,9 +86,6 @@ class App extends React.Component {
         <div>
           <Header />
 
-          <Route exact path="/" component={Dashboard} />
-          <Route path="/topics" component={Topics} />
-
           <Pagination currentPage={this.state.currentPage} totalPages={this.state.totalPages} />
 
           <div className="message">{this.state.message}</div>
@@ -101,6 +96,8 @@ class App extends React.Component {
 
           <Footer />
 
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/topics" component={Topics} />
         </div>
       </Router>
     )
@@ -126,187 +123,28 @@ function MovieList(props) {
 }
 
 const Dashboard = () => <h2>Dashboard</h2>;
+const Topic = ({ match }) => <h3>Requested Param: {match.params.id}</h3>;
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
 
-class Topic extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // search: '',
-      // nowPlaying: true,
-      // popular: false,
-      // topRated: false,
-      // message: '',
-      // currentPage: 0,
-      // totalPages: 0,
-      // imageConfig: {},
-      // movies: [],
-      // // totalResults: 0,
-    };
-  }
+    <ul>
+      <li>
+        <Link to={`${match.url}/components`}>Components</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+      </li>
+    </ul>
 
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  getMovies() {
-    console.log('getting movies')
-    // highest rated: certification_country=US&sort_by=vote_average.desc
-    // in theater: primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22
-
-    console.log(this.props.match.params.id)
-
-    let params = {
-      api_key: '4268bd97bf4cfe10c3797b864eef07b8',
-      language: 'en-US',
-      sort_by: 'popularity.desc', // popular
-      include_adult: false,
-      include_video: false,
-      // with_genres: 878,
-      page: 1,
-      // ID: 12345
-    }
-
-    if (this.props.match.params.id == 'highest-rated') {
-      params.certification_country = 'US';
-      params.sort_by = 'vote_average.desc';
-    }
-    // if {
-
-    // }
-
-    axios.get('https://api.themoviedb.org/3/discover/movie', {params})
-      .then((response) => {
-        console.log('movies: ');
-        console.log(response.data.results);
-        if (response.status === 200) {
-          this.setState({imageConfig: response.data.images})
-          console.log("Image config:")
-          console.log(this.state.imageConfig)
-        } else {
-          console.log('error!');
-        }
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
-      .then(function () {
-        // always executed
-      });
-  }
-
-  componentDidUpdate(prevProps) {
-    const locationChanged = this.props.location !== prevProps.location;
-    // console.log(locationChanged)
-
-    if (locationChanged) {
-      this.getMovies();
-    }
-  }
-
-  render() {
-    return (
-      <h3>Requested Param: {this.props.match.params.id}</h3>
-    );
-  }
-}
-
-// const Topic = ({ match }) => <h3>Requested Param: {match.params.id}</h3>;
-
-// const Topics = ({ match }) => (
-//   <div>
-//     <h2>Topics</h2>
-
-//     <ul>
-//       <li>
-//         <Link to={`${match.url}/components`}>Components</Link>
-//       </li>
-//       <li>
-//         <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-//       </li>
-//     </ul>
-
-//     <Route path={`${match.path}/:id`} component={Topic} />
-//     <Route
-//       exact
-//       path={match.path}
-//       render={() => <h3>Please select a topic.</h3>}
-//     />
-//   </div>
-// );
-
-// function getMovies() {
-//   console.log('got movies')
-//   axios.get('https://api.themoviedb.org/3/discover/movie', {
-//       params: {
-//         api_key: '4268bd97bf4cfe10c3797b864eef07b8',
-//         language: 'en-US',
-//         sort_by: 'popularity.desc', // popular
-//         include_adult: false,
-//         include_video: false,
-//         // with_genres: 878,
-//         page: 1,
-//         // ID: 12345
-//       }
-//     })
-//     .then((response) => {
-//       console.log(response);
-//       if (response.status === 200) {
-//         this.setState({imageConfig: response.data.images})
-//         console.log("Image config:")
-//         console.log(this.state.imageConfig)
-//       } else {
-//         console.log('error!');
-//       }
-//     })
-//     .catch(function (error) {
-//       console.log(error);
-//     })
-//     .then(function () {
-//       // always executed
-//     });
-// }
-
-class Topics extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // search: '',
-      // nowPlaying: true,
-      // popular: false,
-      // topRated: false,
-      // message: '',
-      // currentPage: 0,
-      // totalPages: 0,
-      // imageConfig: {},
-      // movies: [],
-      // // totalResults: 0,
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <h2>Topics</h2>
-
-        <ul>
-          <li>
-            <Link to={`${this.props.match.url}/popular`}>Popular</Link>
-          </li>
-          <li>
-            <Link to={`${this.props.match.url}/highest-rated`}>Highest Rated</Link>
-          </li>
-        </ul>
-
-        <Route path={`${this.props.match.path}/:id`} component={Topic} />
-        <Route
-          exact
-          path={this.props.match.path}
-          render={() => <h3>Please select a topic.</h3>}
-        />
-      </div>
-    );
-  }
-}
+    <Route path={`${match.path}/:id`} component={Topic} />
+    <Route
+      exact
+      path={match.path}
+      render={() => <h3>Please select a topic.</h3>}
+    />
+  </div>
+);
 
 class Pagination extends React.Component {
   render() {
